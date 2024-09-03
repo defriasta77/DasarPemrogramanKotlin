941de0c9f28a9233b6e6dd3749f0ba10a7027701
DEFRIASTARIDO <defri.astarido77@gmail.com>
2024-09-03 14:19:15 +0700

    add TipeData.kt

--- src/main/kotlin/VarvsVal.kt
+++ src/main/kotlin/VarvsVal.kt
@@ -4,4 +4,5 @@ fun main(args: Array<String>) {
     println("Pemberian nilai pertama pada varVariabel = " + varVariabel)
     varVariabel = 78 // assigning second value to varVariabel
     println("Pemberian nilai kedua pada varVariabel = " +varVariabel)
+
 }
