313437e01c134844f7951b2365e5926d40689c7c
DEFRIASTARIDO <defri.astarido77@gmail.com>
2024-09-03 13:45:20 +0700

    add BlokdanKomentar.kt

---
+++ src/main/kotlin/BlokdanKomentar.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Halo, ini adalah program pertama saya di Kotlin")
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
