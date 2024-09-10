54fc2443bca9367618217ae87866382f9403f6ec
DEFRIASTARIDO <defri.astarido77@gmail.com>
2024-09-10 08:35:43 +0700

    add PerintahMasukkanKotlin.kt

--- src/main/kotlin/PerintahMasukkanKotlin.kt
+++ src/main/kotlin/PerintahMasukkanKotlin.kt
@@ -8,3 +8,16 @@ fun main(args: Array<String>) {
     output = input + ", " + input + "!"
     println(output)
 }
+/*
+    //Doubler program parsing
+    println("Enter a number and I'll double it: ")
+    val input = readLine()!!
+    var a = input.toInt() // eventually Double
+    a = a * 2
+    println(a)
+    println("Enter a number, and I'll square it:")
+    val number = readLine()!!.toInt()
+    val square = number * number
+    println("Result: " + square)
+}
+*/
