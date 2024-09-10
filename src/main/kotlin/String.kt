724a3676e0687ad6ffc5cde476b795c748938c31
DEFRIASTARIDO <defri.astarido77@gmail.com>
2024-09-10 08:17:48 +0700

    add String.kt

--- src/main/kotlin/String.kt
+++ src/main/kotlin/String.kt
@@ -13,4 +13,11 @@ for (c in "foo")
 print(c)
 """
     println(text)
+    """
+|Tell me and I forget.
+|Teach me and I remember.
+|Involve me and I learn.
+|(Benjamin Franklin)
+""".trimMargin()
+    println(text)
 }
