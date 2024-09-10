4851a5e68ee781afe0e29d2a3cdbebbbe0fdd6d7
DEFRIASTARIDO <defri.astarido77@gmail.com>
2024-09-10 12:03:56 +0700

    add BilanganDecimal.kt

---
+++ src/main/kotlin/BilanganDecimal.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val maxFloat : Float = 9.1234567f // 6 digit (kelebihan digit akan dibulatkan
+    val maxDouble : Double = 9.123456789 // 10 digit
+
+    println("maxFloat : " + maxFloat)
+    println("maxDouble" +maxDouble)
+}
--- src/main/kotlin/VarvsVal.kt
+++ src/main/kotlin/VarvsVal.kt
@@ -1,8 +1,11 @@
-fun main(args: Array<String>) {
+fun main(args: Array<String>){
     var varVariabel: Int
-    varVariabel = 56 // assigning first value to varVariable
-    println("Pemberian nilai pertama pada varVariabel = " + varVariabel)
-    varVariabel = 78 // assigning second value to varVariabel
-    println("Pemberian nilai kedua pada varVariabel = " +varVariabel)
-
+    varVariabel = 56 //assigning first value to varvariabel
+    println("Pemberian nilai pertama pada varVariabel= "+varVariabel)
+    varVariabel = 78 //assigning second value to varvariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+    var phi = 3.14  //phi dari val diubah ke var,sehingga program bisa dijalankan
+    println("Nilai awal phi ="+phi)//disini jg saya tambah pritnln agar nilai phi dapat dicetak
+    phi = 2.5
+    println("Nilai baru phi ="+phi)
 }
